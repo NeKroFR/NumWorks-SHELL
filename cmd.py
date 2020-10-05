cb558a9f66fc1bb804d6407aeed9feecbe3f3bad
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2020-10-05 09:17:52 +0200

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -1,17 +1,21 @@
+#creates commands
+
 def HelloWorld():
     return "HELLO WORLD"
 
 def CMD2():
-    return "CECI EST UNE DEUXIEME COMMANDE"
+    return "This is a secound command"
 
-def CMD3():
-    return "CECI EST UNE TROISIEME COMMANDE"
+def git():
+    return "Thx NeKro for this software "
+
+def UwU():
+    return "UwU "
 
 
 
 
-
-
+#return commands
 
 while True:
 
@@ -23,8 +27,24 @@ while True:
     elif check.lower() == "cmd2":
         print (CMD2())
 
-    elif check.lower() == "cmd3":
-        print (CMD3())
+    elif check.lower() == "git":
+        print (git())
+        
+    elif check.lower() == "uwu":
+        print (UwU())
+#you can take several spellings for one function
 
+    elif check.lower() == "UWU":
+        print (UwU())
+
+     elif check.lower() == "UwU":
+        print (UwU())
+     
+    
+      elif check.lower() == "q":
+        break
+    
     elif check.lower() == "quit":
         break
+        
+#UwU

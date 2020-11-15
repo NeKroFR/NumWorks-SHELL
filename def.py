b7aab3f269eef03bb4b31eed0fb581e795e52fab
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2020-11-15 17:15:11 +0100

    Update def.py

--- def.py
+++ def.py
@@ -1,30 +1,4 @@
-while True:
-
-    check = input("")
-    if check.lower() == "hi":
-        print (HelloWorld())
-    
-    elif check.lower() == "cmd":
-        print (CMD())
-#you can take several spellings for one function
-    elif check.lower() == "COMMAND":
-        print (CMD())
-
-    elif check.lower() == "thx":
-        print (thx())
-    
-
-    elif check.lower() == "uwu":
-        print (UwU())
-     
-    elif check.lower() == "h":
-        print (index())
-
-    elif check.lower() == "help":
-        print (index())
-
-    elif check.lower() == "q":
-        break
-    
-    elif check.lower() == "quit":
-        break
+def test ():
+ return 'ceci est un test'
+def test2():
+ return 'ceci est un second test'

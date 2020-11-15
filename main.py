e4ebb7e5c602daf58f0490db499c8b60394ed491
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2020-11-15 17:16:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Created by NeKro
 word = str(input("entrer le mot de voc:")) 
 definition = str(input("entrer la defs:"))
 word_file = open('word.py', "a")
-word_file.write("elif check.lower() == ' "+ word +"':\n print ("+ word +"())'"+ word +"'\n\n")
+word_file.write("elif check.lower() == '"+ word +"':\n print ("+ word +"())'"+ word +"'\n\n")
 word_file.close()
 def_file = open('def.py', "a")
 def_file.write("def "+ word +"():\n return '"+ definition +"'\n\n")

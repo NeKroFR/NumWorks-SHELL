90c27220506681f21c8f244b51650d2521d9fc4c
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2020-11-15 17:13:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-definition = str(input("entrer les defs:")) 
+'''
+Created by NeKro
+'''
+word = str(input("entrer le mot de voc:")) 
+definition = str(input("entrer la defs:"))
+word_file = open('word.py', "a")
+word_file.write("elif check.lower() == ' "+ word +"':\n print ("+ word +"())'"+ word +"'\n\n")
+word_file.close()
 def_file = open('def.py', "a")
-def_file.write("def "+ definition +"():\n return '"+ definition +"'\n")
+def_file.write("def "+ word +"():\n return '"+ definition +"'\n\n")
 def_file.close()

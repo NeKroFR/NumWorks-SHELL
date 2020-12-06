f1dc861673eb0ec34dbabbc7ca5d864ef5165f09
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2020-12-06 14:02:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 '''
 Created by NeKro
 '''
-word = str(input("entrer le mot de voc:")) 
-definition = str(input("entrer la defs:"))
-word_file = open('word.py', "a")
-word_file.write("elif check.lower() == '"+ word +"':\n print ("+ word +"())'"+ word +"'\n\n")
-word_file.close()
-def_file = open('def.py', "a")
-def_file.write("def "+ word +"():\n return '"+ definition +"'\n\n")
-def_file.close()
+while True:
+    choice = input("\najouter un nouveau mot : 0\ncompiler : 1\nquit : q\n")
+    if choice == "0":
+        word = str(input("entrer le mot de voc:")) 
+        definition = str(input("entrer la defs:"))
+        word_file = open('word.py', "a")
+        word_file.write("elif check.lower() == '"+ word +"':\n print ("+ word +"())'"+ word +"'\n\n")
+        word_file.close()
+        def_file = open('def.py', "a")
+        def_file.write("def "+ word +"():\n return '"+ definition +"'\n\n")
+        def_file.close()
+
+    if choice == "1":
+        print ("compile\n")
+
+    if choice == "q":
+        break
+    if choice == "Q":
+        break
+
